6a665a6846f85bb8c7934f68b1672203a3067973
lucastso <lucastassisouza@gmail.com>
2022-03-13 21:32:29 -0300

    feat: basic components

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -4,7 +4,22 @@ import "./styles.css";
 export function Nav() {
   return (
     <div className="nav-container">
-      <div className=""></div>
+      <div className="">logo</div>
+      <div className="nav-options">
+        <a href="#" className="nav-option">
+          Sobre
+        </a>
+        <a href="#" className="nav-option">
+          Serviços
+        </a>
+        <a href="#" className="nav-option">
+          Projetos
+        </a>
+        <a href="#" className="nav-option">
+          Contato
+        </a>
+      </div>
+      <button className="nav-start">Começar</button>
     </div>
   );
 }
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,16 @@
 import React from "react";
+import { Nav } from "../../components/Nav";
 
 export function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Nav />
+      <div className="home-text">
+        <h1 className="home-headtext">Móveis para viver melhor.</h1>
+        <p className="home-ptext">
+          Texto aqui, algo para servir de referência e afins.
+        </p>
+      </div>
+    </div>
+  );
 }
