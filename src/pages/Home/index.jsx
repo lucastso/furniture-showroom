653688001b3ebcc9215710b8015127506ab213e4
lucastso <lucastassisouza@gmail.com>
2022-07-22 14:11:01 -0300

    feat: new image

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -26,137 +26,146 @@ export function Home() {
         >
           <img src={zap} alt="whatsapp logo" width={36} />
         </a>
-          <div className="home-text">
-            <h1 className="home-headtext">
-              Interiores{" "}
-              <span
-                style={{
-                  background: "#1b1b1b",
-                  paddingLeft: "1rem",
-                  paddingRight: "1rem",
-                  color: "white",
-                }}
-              >
-                modernos.
-              </span>
-            </h1>
-            <p className="home-ptext">
-              Feitos com excelência. Transforme seu interior com móveis super
-              agradáveis.
+        <div className="home-text">
+          <h1 className="home-headtext">
+            Interiores{" "}
+            <span
+              style={{
+                background: "#1b1b1b",
+                paddingLeft: "1rem",
+                paddingRight: "1rem",
+                color: "white",
+              }}
+            >
+              modernos.
+            </span>
+          </h1>
+          <p className="home-ptext">
+            Feitos com excelência. Transforme seu interior com móveis super
+            agradáveis.
+          </p>
+        </div>
+        <Parallax
+          bgImage="https://i.imgur.com/bxB3siQ.jpg"
+          bgImageAlt="the furnitures"
+          strength={100}
+        >
+          <p className="home-parallax"></p>
+        </Parallax>
+        <div className="home-after-image1">
+          <div className="home-main-bg1"></div>
+          <div className="home-main1">
+            <h1 className="home-main-title">Beleza, delicadeza e elegância</h1>
+            <p className="home-main-sub1">
+              Toda casa precisa de uma atmosfera confortável. Consideramos todos
+              os detalhes para criar um ambiente com um design dos sonhos e
+              aconchegante, que você sentirá falta após um longo dia de
+              trabalho. Faça seu cômodo moderno.
             </p>
           </div>
-          <Parallax
-            bgImage="https://i.imgur.com/Y40gL3n.jpg"
-            bgImageAlt="the furnitures"
-            strength={100}
-          >
-            <p className="home-parallax"></p>
-          </Parallax>
-          <div className="home-after-image1">
-            <div className="home-main-bg1"></div>
-            <div className="home-main1">
-              <h1 className="home-main-title">
-                Beleza, delicadeza e elegância
-              </h1>
-              <p className="home-main-sub1">
-                Toda casa precisa de uma atmosfera confortável. Consideramos
-                todos os detalhes para criar um ambiente com um design dos
-                sonhos e aconchegante, que você sentirá falta após um longo dia
-                de trabalho. Faça seu cômodo moderno.
-              </p>
+        </div>
+        <div className="home-after-image2">
+          <div className="home-main2">
+            <h1 className="home-main-title">Nossa decoração</h1>
+            <p className="home-main-sub2">
+              Somos apaixonados em fazer nossa parte para nossos clientes terem
+              uma vida melhor e mais tranquila. Conectamos pessoas e os produtos
+              de uma forma indiscutível. Criamos histórias e cômodos para todos.
+            </p>
+          </div>
+          <div className="home-main-bg2"></div>
+        </div>
+        <Parallax
+          bgImage="https://i.imgur.com/Y40gL3n.jpg"
+          bgImageAlt="the furnitures"
+          strength={100}
+        >
+          <p className="home-parallax2"></p>
+        </Parallax>
+        <div className="home-after-image3">
+          <div className="home-main3">
+            <h1 className="home-main-title">A Cumaru</h1>
+            <p className="home-main-sub2">
+              Nossa loja oferece móveis marcantes, criados com uma combinação de
+              conforto, funcionalidade e acessibilidade. Desde móveis para a
+              sala de estar, a cozinha e para o quarto. Nossa coleção é decidida
+              para criar um espaço que combine com o seu estilo de vida.
+            </p>
+          </div>
+          <div className="home-main3">
+            <h1 className="home-main-title">Para você</h1>
+            <p className="home-main-sub2">
+              Um ar de elegância e autenticidade é trazido para todo o espaço.
+              Sinta-se, mais do que nunca, em casa.
+            </p>
+          </div>
+        </div>
+        <div className="home-photos">
+          <div className="home-photo1">
+            <div className="home-box-photo">
+              <img src={furni} alt="furniture icon" width={16} />
+              <p className="home-text-insidebox">Sofá bege</p>
             </div>
           </div>
-          <div className="home-after-image2">
-            <div className="home-main2">
-              <h1 className="home-main-title">Nossa decoração</h1>
-              <p className="home-main-sub2">
-                Somos apaixonados em fazer nossa parte para nossos clientes
-                terem uma vida melhor e mais tranquila. Conectamos pessoas e os
-                produtos de uma forma indiscutível. Criamos histórias e cômodos
-                para todos.
-              </p>
-            </div>
-            <div className="home-main-bg2"></div>
-          </div>
-          <Parallax
-            bgImage="https://i.imgur.com/Y40gL3n.jpg"
-            bgImageAlt="the furnitures"
-            strength={100}
-          >
-            <p className="home-parallax2"></p>
-          </Parallax>
-          <div className="home-after-image3">
-            <div className="home-main3">
-              <h1 className="home-main-title">A Cumaru</h1>
-              <p className="home-main-sub2">
-                Nossa loja oferece móveis marcantes, criados com uma combinação
-                de conforto, funcionalidade e acessibilidade. Desde móveis para
-                a sala de estar, a cozinha e para o quarto. Nossa coleção é
-                decidida para criar um espaço que combine com o seu estilo de
-                vida.
-              </p>
-            </div>
-            <div className="home-main3">
-              <h1 className="home-main-title">Para você</h1>
-              <p className="home-main-sub2">
-                Um ar de elegância e autenticidade é trazido para todo o espaço.
-                Sinta-se, mais do que nunca, em casa.
-              </p>
+          <div className="home-photo2">
+            <div className="home-box-photo">
+              <img src={furni} alt="furniture icon" width={16} />
+              <p className="home-text-insidebox">Mesa e cadeira de madeira</p>
             </div>
           </div>
-          <div className="home-photos">
-            <div className="home-photo1">
-              <div className="home-box-photo">
-                <img src={furni} alt="furniture icon" width={16} />
-                <p className="home-text-insidebox">Sofá bege</p>
-              </div>
-            </div>
-            <div className="home-photo2">
-              <div className="home-box-photo">
-                <img src={furni} alt="furniture icon" width={16} />
-                <p className="home-text-insidebox">Mesa e cadeira de madeira</p>
-              </div>
-            </div>
-            <div className="home-photo3">
-              <div className="home-box-photo">
-                <img src={furni} alt="furniture icon" width={16} />
-                <p className="home-text-insidebox">Criado mudo claro</p>
-              </div>
+          <div className="home-photo3">
+            <div className="home-box-photo">
+              <img src={furni} alt="furniture icon" width={16} />
+              <p className="home-text-insidebox">Criado mudo claro</p>
             </div>
           </div>
-          <footer className="home-footer">
-            <img src={logo2} alt="logo" width={240} />
-            <section className="home-contatos1">
-              <div className="home-flexible-options">
-                <img src={pin} alt="pin" width={12} />
-                <a className="home-option" href="https://goo.gl/maps/qzzUeMtLcnuCowum6" target="_blank">
-                  Avenida Zélia de Lima Rosa, 454.
-                  <strong> Boituva, SP</strong>
-                </a>
-              </div>
-              <div className="home-flexible-options">
-                <img src={tele} alt="tele" width={18} />
-                <a className="home-option" href="tel:+5515997572370"> (11) 3264-9800</a>
-              </div>
-              <div className="home-flexible-options">
-                <img src={email} alt="tele" width={18} />
-                <a className="home-option" href="mailto:cumarumoveisedecor@hotmail.com">cumarumoveisedecor@hotmail.com</a>
-              </div>
-            </section>
-            <section className="home-contatos2">
-              <div className="home-flexible-options">
-                <img src={face2} alt="facebook logo" width={22} />
-                <p className="home-option">Cumaru móveis</p>
-              </div>
+        </div>
+        <footer className="home-footer">
+          <img src={logo2} alt="logo" width={240} />
+          <section className="home-contatos1">
+            <div className="home-flexible-options">
+              <img src={pin} alt="pin" width={12} />
               <a
-                className="home-flexible-options"
-                href="https://www.instagram.com/cumarumoveisedecoracao/" target="_blank"
+                className="home-option"
+                href="https://goo.gl/maps/qzzUeMtLcnuCowum6"
+                target="_blank"
               >
-                <img src={insta2} alt="instagram logo" width={22} />
-                <p className="home-option">@cumarumoveisedecoracao</p>
+                Avenida Zélia de Lima Rosa, 454.
+                <strong> Boituva, SP</strong>
               </a>
-            </section>
-          </footer>
+            </div>
+            <div className="home-flexible-options">
+              <img src={tele} alt="tele" width={18} />
+              <a className="home-option" href="tel:+5515997572370">
+                {" "}
+                (11) 3264-9800
+              </a>
+            </div>
+            <div className="home-flexible-options">
+              <img src={email} alt="tele" width={18} />
+              <a
+                className="home-option"
+                href="mailto:cumarumoveisedecor@hotmail.com"
+              >
+                cumarumoveisedecor@hotmail.com
+              </a>
+            </div>
+          </section>
+          <section className="home-contatos2">
+            <div className="home-flexible-options">
+              <img src={face2} alt="facebook logo" width={22} />
+              <p className="home-option">Cumaru móveis</p>
+            </div>
+            <a
+              className="home-flexible-options"
+              href="https://www.instagram.com/cumarumoveisedecoracao/"
+              target="_blank"
+            >
+              <img src={insta2} alt="instagram logo" width={22} />
+              <p className="home-option">@cumarumoveisedecoracao</p>
+            </a>
+          </section>
+        </footer>
       </section>
 
       {/* MOBILE SECTION */}
@@ -271,18 +280,30 @@ export function Home() {
           <section className="home-contatos1">
             <div className="home-flexible-options">
               <img src={pin} alt="pin" width={12} />
-              <a className="home-option" href="https://goo.gl/maps/qzzUeMtLcnuCowum6" target="_blank">
+              <a
+                className="home-option"
+                href="https://goo.gl/maps/qzzUeMtLcnuCowum6"
+                target="_blank"
+              >
                 Avenida Zélia de Lima Rosa, 454.
                 <strong> Boituva, SP</strong>
               </a>
             </div>
             <div className="home-flexible-options">
               <img src={tele} alt="tele" width={18} />
-              <a className="home-option" href="tel:+5515997572370"> (11) 3264-9800</a>
+              <a className="home-option" href="tel:+5515997572370">
+                {" "}
+                (11) 3264-9800
+              </a>
             </div>
             <div className="home-flexible-options">
               <img src={email} alt="tele" width={18} />
-              <a className="home-option" href="mailto:cumarumoveisedecor@hotmail.com">cumarumoveisedecor@hotmail.com</a>
+              <a
+                className="home-option"
+                href="mailto:cumarumoveisedecor@hotmail.com"
+              >
+                cumarumoveisedecor@hotmail.com
+              </a>
             </div>
           </section>
           <section className="home-contatos2">
@@ -292,7 +313,8 @@ export function Home() {
             </div>
             <a
               className="home-flexible-options"
-              href="https://www.instagram.com/cumarumoveisedecoracao/" target="_blank"
+              href="https://www.instagram.com/cumarumoveisedecoracao/"
+              target="_blank"
             >
               <img src={insta2} alt="instagram logo" width={22} />
               <p className="home-option">@cumarumoveisedecoracao</p>
